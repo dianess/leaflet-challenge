c2ec585336d6eb3286cd90eacf29456eb3df8c28
dianess <analystdiane@gmail.com>
2019-12-13 14:52:40 -0700

    change markers to circles

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,30 +4,67 @@
 
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
 
+// Function to determine marker size based on earthquake magnitude
+function markerSize(feature) {
+  //return Math.sqrt(Math.abs(feature.properties.mag)) * 5;
+  return Math.pow(2, magnitude) / 2;
+}
+
+// Function to set colors based on the earthquake magnitude
+function getColor(d) {
+  return d > 9? '#FF0000' :
+        d > 8? '#FA8072' :
+        d > 7  ? '#eede9f' :
+        d > 6  ? '#FF8C00' :
+        d > 5   ? '#dfedbe' :
+                   '#7FFF00';
+ }
+
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
-});
+}); //ends d3.json
 
 function createFeatures(earthquakeData) {
 
   // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
+  // Give each feature a popup describing the magniture, place, and time of the earthquake
   function onEachFeature(feature, layer) {
     layer.bindPopup('<div align="center">' + "<h3>" + "Magnitude: "  + feature.properties.mag + "<p>" + feature.properties.place + "</p>" +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p></div>");
-  }
+  }  //ends function onEachFeature
+
+  // Works with pointToLayer below to provide circles with certain colors 
+  // from the function above: getColor
+  function style(feature) {
+    var mag = feature.properties.mag; 
+    var color_value=getColor(mag)
+  
+    return {radius: feature.properties.mag*2,
+      color: "#000",
+      fillColor:color_value,
+      fillOpacity: 0.8,
+      weight: 1,
+      opacity: 0}
+    }
   
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+    onEachFeature: onEachFeature,
+
+    // Works with the function style and to change the default markers to circles
+    // with specific colors
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, style(feature));
+    },
+
+  });  //ends var earthquakes
 
   // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
-}
+} //ends function createFeatures
 
 function createMap(earthquakes) {
 
@@ -72,4 +109,4 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
-}
+};  //ends function createMap
